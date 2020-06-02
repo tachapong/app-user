3cc345979cdc8afce9686091e299395bfd225d63
tachapong <tachapong999@gmail.com>
2020-06-02 18:53:59 +0700

    ui user-app

--- src/app/delivery-cancel-confirm/delivery-cancel-confirm.page.ts
+++ src/app/delivery-cancel-confirm/delivery-cancel-confirm.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-delivery-cancel-confirm',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delivery-cancel-confirm.page.scss'],
 })
 export class DeliveryCancelConfirmPage implements OnInit {
+  public fg: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.fg = this.fb.group({
+      'heading': [null, Validators.required],
+      'info': null,
+    })
+  }
 
   ngOnInit() {
   }
 
+  handleSubmit() {
+    console.log("ok");
+    
+  }
+
+ 
 }
