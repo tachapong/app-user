54127597c0b5b2f67d8dfdccb9a0badb2f677a2b
tachapong <tachapong999@gmail.com>
2020-06-03 16:37:54 +0700

    implement service and interface

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false
 };
 
+export const API_URL = 'https://delivery-3rd-api.azurewebsites.net/api/Mana3rd/';
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
---
+++ src/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { IUserService } from './iuser';
+import { API_URL } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService implements IUserService {
+
+  async getCurrentlyOpenedRestaurants(): Promise<any> {
+    let apiUrl = this.baseUrl + "GetCurrentlyOpenedRestaurants/";
+    return this.http.get(apiUrl).toPromise();
+  }
+
+  async getRestaurantMenu(restaurantId: string): Promise<any> {
+    let apiUrl = this.baseUrl + "GetRestaurantMenu/" + restaurantId;
+    return this.http.get(apiUrl).toPromise();
+  }
+
+  async getProductDetail(productId: string): Promise<any> {
+    let apiUrl = this.baseUrl + "GetProductDetail/" + productId;
+    return this.http.get(apiUrl).toPromise();
+  }
+
+  async getOwnOrder(orderId: string): Promise<any> {
+    let apiUrl = this.baseUrl + "GetOwnOrder/" + orderId;
+    return this.http.get(apiUrl).toPromise();
+  }
+
+  private baseUrl: string = API_URL;
+  constructor(private http: HttpClient) { }
+
+}
