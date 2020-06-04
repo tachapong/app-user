74080a8d68b667cd51d7065ceb6cbf2f4a3dfa8d
powerofwind <powerofwind10@gmail.com>
2020-06-04 11:30:01 +0700

    binding delivery order detail page

--- src/app/delivery-order-detail/delivery-order-detail.page.ts
+++ src/app/delivery-order-detail/delivery-order-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-delivery-order-detail',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryOrderDetailPage implements OnInit {
 
-  constructor() { }
+  public order$ = Promise.resolve([]);
+  constructor(private userSvc: UserService) { }
+
+  ionViewDidEnter() {
+    this.getOwnOrder()
+  }
 
   ngOnInit() {
+  }
+
+  getOwnOrder() {
+    this.order$ = this.userSvc.getOwnOrder("string");
+    this.order$.then(it => {
+      console.log(it);
+    })
   }
 
 }
