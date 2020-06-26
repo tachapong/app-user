a248892d75995ae7757012fef56356e242311eea
perawatt <pekung4433@perfenterprise.com>
2020-06-26 16:50:13 +0700

    implement initpage shop-select

--- src/app/shop-select/shop-select.page.ts
+++ src/app/shop-select/shop-select.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/services/user.service';
+import { IonManaLib } from 'ion-m-lib';
 
 @Component({
   selector: 'app-shop-select',
@@ -8,16 +9,33 @@ import { UserService } from 'src/services/user.service';
 })
 export class ShopSelectPage implements OnInit {
 
-  openedRestaurantList$ = Promise.resolve([]);
-  
-  constructor(private userSvc: UserService) { }
+  // openedRestaurantList$ = Promise.resolve([]);
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  mcontentid = "637287538894910727"
+  constructor(private userSvc: UserService, private svc: IonManaLib) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
-    this.openedRestaurantList$ = this.userSvc.getCurrentlyOpenedRestaurants();
-    this.openedRestaurantList$.then((it)=>{console.log(it);});
+    // this.openedRestaurantList$ = this.userSvc.getCurrentlyOpenedRestaurants();
+    // this.openedRestaurantList$.then((it)=>{console.log(it);});
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    // this.data$ = load$;
+    load$.then(it => {
+      console.log(it);      
+      this.svc.initPageApi(this.mcontentid);
+      // this.hasLoaded = it ? "y" : "n";
+    });
+  }
+  
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        // return this.svc.getApiData(this.mcontentid);
+      })
   }
 
 }
