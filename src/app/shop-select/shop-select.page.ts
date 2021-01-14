7f159f4359bbd6a47d62c3963b38cb5badd5076d
perawatt <pekung4433@perfenterprise.com>
2021-01-14 10:24:17 +0700

    style shop-select

--- src/app/shop-select/shop-select.page.ts
+++ src/app/shop-select/shop-select.page.ts
@@ -32,6 +32,8 @@ export class ShopSelectPage implements OnInit {
   private loadData$() {
     return this.svc.initPageApi(this.mcontentid)
       .then(_ => {
+        return this.svc.callApiGet(this.mcontentid,"http://delivery-3rd-api.azurewebsites.net/api/Mana3rd/GetCurrentlyOpenedRestaurants");
+
         return this.svc.getApiData(this.mcontentid);
       })
   }
