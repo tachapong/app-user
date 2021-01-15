ff8a14fa3627a15a6a8447b3c73f00f1c90d9169
perawatt <pekung4433@perfenterprise.com>
2021-01-15 15:57:58 +0700

    style delivery-cancel-completed

--- src/app/delivery-cancel-completed/delivery-cancel-completed.page.ts
+++ src/app/delivery-cancel-completed/delivery-cancel-completed.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonManaLib } from 'ion-m-lib';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-delivery-cancel-completed',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryCancelCompletedPage implements OnInit {
 
-  constructor() { }
+  public title: string = "Delivery company";
+  public hasLoaded: string;
+  public data$ = Promise.resolve<{}>({});
+  mcontentid = ""
+  constructor(private userSvc: UserService, private svc: IonManaLib) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.hasLoaded = null;
+    let load$ = this.loadData$();
+    this.data$ = load$;
+    load$.then(it => {
+      console.log(it);      
+      this.svc.initPageApi(this.mcontentid);
+      this.hasLoaded = it ? "y" : "n";
+    });
+  }
+  
+  private loadData$() {
+    return this.svc.initPageApi(this.mcontentid)
+      .then(_ => {
+        return this.svc.getApiData(this.mcontentid);
+      })
   }
 
 }
