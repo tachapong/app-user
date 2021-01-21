0b254c684971c7a325341bccd094e9ce97f81962
perawatt <pekung4433@perfenterprise.com>
2021-01-21 18:11:08 +0700

    Change select shop endpoint.

--- src/app/shop-menu-select/shop-menu-select.page.ts
+++ src/app/shop-menu-select/shop-menu-select.page.ts
@@ -53,6 +53,6 @@ export class ShopMenuSelectPage implements OnInit {
   }
 
   onSelect(productId: string) {
-    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/np/npdtext-deliverybanmore$" + productId + "~delivery");
+    this.svc.visitEndpoint(this.mcontentid, "https://s.manal.ink/np/npdtdtl-" + productId + "~delivery");
   }
 }
